a62d77f32cd392032c282530faade71fe3a15f2e
Sathyaprakash Dhanabal <sdhanabal@equalexperts.com>
2019-03-28 14:47:36 +0100

    Date time request param serialization.

--- src/main/java/org/happysathya/route/InformationRoute.java
+++ src/main/java/org/happysathya/route/InformationRoute.java
@@ -4,10 +4,12 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.happysathya.dto.InformationRequest;
 import org.happysathya.dto.InformationResponse;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.ZonedDateTime;
 
 @RestController
 @CrossOrigin(maxAge = 3600)
@@ -19,5 +21,13 @@ public class InformationRoute {
     @ApiOperation(value = "Get information for the provided data")
     public InformationResponse getInformation(@RequestBody @Valid InformationRequest informationRequest) {
         return new InformationResponse();
+    }
+
+    @RequestMapping(value = "/date", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get information for the provided data")
+    public InformationResponse getDate(@RequestParam("departureTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime departureTime) {
+        InformationResponse informationResponse = new InformationResponse();
+        informationResponse.setName(departureTime.toString());
+        return informationResponse;
     }
 }
